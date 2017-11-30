62fc53e6bed30b4c6422c41dac664d26c86be37d
774655048@qq.com <774655048@qq.com>
2017-11-30 15:16:01 +0800

    release 1.0.4

--- RITLKit/RITLKit.h
+++ RITLKit/RITLKit.h
@@ -10,7 +10,22 @@
 #define RITLKit_h
 
 
-#import "RITLCategory.h"
+#import "UIView+RITLExtension.h"
+#import "NSDate+RITLExtension.h"
+#import "NSArray+RITLExtension.h"
+#import "UIColor+RITLExtension.h"
+#import "UIImage+RITLExtension.h"
+#import "NSObject+RITLExtension.h"
+#import "NSNumber+RITLExtension.h"
+#import "NSString+RITLExtension.h"
+#import "UIView+RITLFrameChanged.h"
+#import "UIControl+RITLBlockButton.h"
+#import "NSDictionary+RITLExtension.h"
+#import "UISearchBar+RITLCustomColor.h"
+#import "UITableView+RITLCellRegister.h"
+#import "UIViewController+RITLExtension.h"
+#import "CAGradientLayer+RITLGradientLayer.h"
+#import "UINavigationController+RITLExtension.h"
 #import "RITLEXTkeyConfig.h"
 
 //Web
